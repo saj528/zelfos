17a1d842ac12366265c693859bbe2dfd11fa0c97
Cody Seibert <cseibert@flexion.us>
2020-09-30 22:33:32 -0400

    direction animations

--- core/src/entities/Player.java
+++ core/src/entities/Player.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Timer;
@@ -40,15 +41,33 @@ public class Player extends Sprite {
     public final float ACCELERATION = 300.0f;
     public final float MAX_SPEED = 10;
 
+    private enum DIRECTIONS {
+        IDLE,
+        UP,
+        DOWN,
+        RIGHT,
+        LEFT
+    }
+
     private Texture playerUp = new Texture("player-up.png");
     private Texture playerDown = new Texture("player-down.png");
     private Texture playerLeft = new Texture("player-left.png");
     private Texture playerRight = new Texture("player-right.png");
     private boolean canAttack = true;
+    private boolean isRunningLeft = false;
+    private boolean isRunningRight = false;
+    private boolean isRunningUp = false;
+    private boolean isRunningDown = false;
     private float ATTACK_OFFSET = 0.05f;
     private int SWORD_DAMAGE = 1;
     private Animation<Texture> attackAnimation;
+    private Animation<TextureRegion> downAnimation;
+    private Animation<TextureRegion> leftAnimation;
+    private Animation<TextureRegion> upAnimation;
+    private Animation<TextureRegion> rightAnimation;
     private float attackTime = 0f;
+    private float walkTime = 0f;
+
     private float ATTACK_ANIMATION_SPEED = 0.025f;
     private float ATTACK_COOLDOWN = ATTACK_ANIMATION_SPEED * 13;
 
@@ -71,35 +90,69 @@ public class Player extends Sprite {
         attackFrames[11] = new Texture("attack/a12.png");
         attackFrames[12] = new Texture("attack/a13.png");
         attackAnimation = new Animation<Texture>(ATTACK_ANIMATION_SPEED, attackFrames);
+
+
+        Texture playerSheet = new Texture("player-sheet.png");
+
+        TextureRegion[][] playerSheetRegions = TextureRegion.split(playerSheet,
+                playerSheet.getWidth() / 4,
+                playerSheet.getHeight() / 3);
+        TextureRegion[][] playerSheetRegions2 = TextureRegion.split(playerSheet,
+                playerSheet.getWidth() / 4,
+                playerSheet.getHeight() / 3);
+
+        TextureRegion[] downFrames = new TextureRegion[3];
+        downFrames[0] = playerSheetRegions[0][0];
+        downFrames[1] = playerSheetRegions[0][1];
+        downFrames[2] = playerSheetRegions[0][2];
+        downAnimation = new Animation<TextureRegion>(0.15f, downFrames);
+
+        TextureRegion[] leftFrames = new TextureRegion[3];
+        leftFrames[0] = playerSheetRegions[2][0];
+        leftFrames[1] = playerSheetRegions[2][1];
+        leftFrames[2] = playerSheetRegions[2][2];
+        leftAnimation = new Animation<TextureRegion>(0.15f, leftFrames);
+
+        TextureRegion[] upFrames = new TextureRegion[3];
+        upFrames[0] = playerSheetRegions[1][0];
+        upFrames[1] = playerSheetRegions[1][1];
+        upFrames[2] = playerSheetRegions[1][2];
+        upAnimation = new Animation<TextureRegion>(0.15f, upFrames);
+
+        TextureRegion[] rightFrames = new TextureRegion[3];
+        rightFrames[0] = playerSheetRegions2[2][0];
+        rightFrames[1] = playerSheetRegions2[2][1];
+        rightFrames[2] = playerSheetRegions2[2][2];
+        rightFrames[0].flip(true, false);
+        rightFrames[1].flip(true, false);
+        rightFrames[2].flip(true, false);
+        rightAnimation = new Animation<TextureRegion>(0.15f, rightFrames);
     }
 
-    public void updatePlayerMovement(boolean left, boolean right, boolean up,boolean down, float delta){
+    public void updatePlayerMovement(boolean left, boolean right, boolean up, boolean down, float delta) {
         input_vector.x = (right ? 1 : 0) - (left ? 1 : 0);
         input_vector.y = (up ? 1 : 0) - (down ? 1 : 0);
+
+        if (down) {
+            isRunningDown = true;
+        }
+
+        if (left) {
+            isRunningLeft = true;
+        }
+
+        if (right) {
+            isRunningRight = true;
+        }
+
+        if (up) {
+            isRunningUp = true;
+        }
+
         setX(getX() + input_vector.x * ACCELERATION * delta);
         setY(getY() + input_vector.y * ACCELERATION * delta);
     }
-/*
-    public void walkLeft() {
-        setTexture(playerLeft);
-        setX(getX() - SPEED);
-    }
 
-    public void walkRight() {
-        setTexture(playerRight);
-        setX(getX() + SPEED);
-    }
-
-    public void walkDown() {
-        setTexture(playerDown);
-        setY(getY() - SPEED);
-    }
-
-    public void walkUp() {
-        setTexture(playerUp);
-        setY(getY() + SPEED);
-    }
-*/
     public void attack(final ArrayList<Enemy> enemies) {
         if (!canAttack) return;
         canAttack = false;
@@ -144,6 +197,11 @@ public class Player extends Sprite {
 
     public void update(float delta) {
         attackTime += delta;
+        walkTime += delta;
+        isRunningDown = false;
+        isRunningUp = false;
+        isRunningLeft = false;
+        isRunningRight = false;
     }
 
     @Override
@@ -153,7 +211,17 @@ public class Player extends Sprite {
         if (!canAttack) {
             batch.draw(attackAnimation.getKeyFrame(attackTime), getX(), getY(), 64, 64);
         } else {
-            super.draw(batch);
+            if (isRunningLeft) {
+                batch.draw(leftAnimation.getKeyFrame(walkTime, true), getX(), getY());
+            } else if (isRunningRight) {
+                batch.draw(rightAnimation.getKeyFrame(walkTime, true), getX(), getY());
+            } else if (isRunningUp) {
+                batch.draw(upAnimation.getKeyFrame(walkTime, true), getX(), getY());
+            } else if (isRunningDown) {
+                batch.draw(downAnimation.getKeyFrame(walkTime, true), getX(), getY());
+            } else {
+                super.draw(batch);
+            }
         }
         Texture box = createHitboxTexture(20, 20);
         AttackHitbox hitbox = new AttackHitbox(this);
