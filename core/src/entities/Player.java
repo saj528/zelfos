21e5e9be6a32addbd1828d1167d60c4a3b90da14
Alex <ajmarksboy@gmail.com>
2020-10-04 17:26:44 -0500

    player attack and walkig animations redux

--- core/src/entities/Player.java
+++ core/src/entities/Player.java
@@ -27,10 +27,10 @@ class AttackHitbox {
         int offset = 50;
         int size = 40;
 
-        up = new Rectangle(player.getX() + 10, player.getY() + offset + 10, size, size);
-        left = new Rectangle(player.getX() + 10 - offset, player.getY() + 10, size, size);
-        right = new Rectangle(player.getX() + 10 + offset, player.getY() + 10, size, size);
-        down = new Rectangle(player.getX() + 10, player.getY() - offset + 10, size, size);
+        up = new Rectangle(player.getX() - 8, player.getY() + 25, size, size);
+        left = new Rectangle(player.getX() - 40, player.getY() - 5, size, size);
+        right = new Rectangle(player.getX() + 20, player.getY() - 5, size, size);
+        down = new Rectangle(player.getX() - 10, player.getY() - offset + 10, size, size);
     }
 
 }
@@ -51,7 +51,7 @@ public class Player extends Sprite {
     }
 
     private Texture playerUp = new Texture("player-up.png");
-    private Texture playerDown = new Texture("player-down.png");
+    private Texture playerDown = new Texture("playersprites/Run Down/run_down_1.png");
     private Texture playerLeft = new Texture("player-left.png");
     private Texture playerRight = new Texture("player-right.png");
     private boolean canAttack = true;
@@ -68,14 +68,14 @@ public class Player extends Sprite {
     private float ATTACK_OFFSET = 0.05f;
     private int SWORD_DAMAGE = 1;
     private Animation<Texture> attackAnimation;
-    private Animation<TextureRegion> downAnimation;
-    private Animation<TextureRegion> leftAnimation;
-    private Animation<TextureRegion> upAnimation;
-    private Animation<TextureRegion> rightAnimation;
-    private TextureRegion attackUp;
-    private TextureRegion attackDown;
-    private TextureRegion attackLeft;
-    private TextureRegion attackRight;
+    private Animation<Texture> downAnimation;
+    private Animation<Texture> leftAnimation;
+    private Animation<Texture> upAnimation;
+    private Animation<Texture> rightAnimation;
+    private Animation<TextureRegion> attackUp;
+    private Animation<TextureRegion> attackDown;
+    private Animation<TextureRegion> attackLeft;
+    private Animation<TextureRegion> attackRight;
     private float animationSpeed = 0.13f;
     private float attackTime = 0f;
     private float walkTime = 0f;
@@ -87,6 +87,8 @@ public class Player extends Sprite {
     private float ATTACK_ANIMATION_DURATION = 0.2f;
     private int bombs = 100;
     private boolean shouldFlashRed;
+    private int attackOffsetX = 11;
+    private int attackOffsetY = 13;
 
     private enum Direction {
         None,
@@ -119,9 +121,10 @@ public class Player extends Sprite {
     }
 
     public Player(float x, float y) {
-        super(new Texture("player-down.png"));
+        super(new Texture("playersprites/Run Down/run_down_1.png"));
         setPosition(x, y);
         setTexture(playerDown);
+        isFacingDown = true;
         Texture[] attackFrames = new Texture[13];
         attackFrames[0] = new Texture("attack/a1.png");
         attackFrames[1] = new Texture("attack/a2.png");
@@ -140,38 +143,81 @@ public class Player extends Sprite {
 
 
         Texture playerSheet = new Texture("player-sheet-all.png");
-
         TextureRegion[][] playerSheetRegions = TextureRegion.split(playerSheet,
                 playerSheet.getWidth() / 4,
                 playerSheet.getHeight() / 4);
 
-        TextureRegion[] downFrames = new TextureRegion[3];
-        downFrames[0] = playerSheetRegions[0][0];
-        downFrames[1] = playerSheetRegions[0][1];
-        downFrames[2] = playerSheetRegions[0][2];
-        downAnimation = new Animation<TextureRegion>(animationSpeed, downFrames);
-        attackDown = playerSheetRegions[0][3];
 
-        TextureRegion[] leftFrames = new TextureRegion[3];
-        leftFrames[0] = playerSheetRegions[2][0];
-        leftFrames[1] = playerSheetRegions[2][1];
-        leftFrames[2] = playerSheetRegions[2][2];
-        leftAnimation = new Animation<TextureRegion>(animationSpeed, leftFrames);
-        attackLeft = playerSheetRegions[2][3];
+        Texture playerAttackSheet = new Texture("playersprites/player_attack_frames.png");
+        TextureRegion[][] playerAttackSheetRegions = TextureRegion.split(playerAttackSheet,
+                playerAttackSheet.getWidth() / 4,
+                playerAttackSheet.getHeight() / 4);
 
-        TextureRegion[] upFrames = new TextureRegion[3];
-        upFrames[0] = playerSheetRegions[1][0];
-        upFrames[1] = playerSheetRegions[1][1];
-        upFrames[2] = playerSheetRegions[1][2];
-        upAnimation = new Animation<TextureRegion>(animationSpeed, upFrames);
-        attackUp = playerSheetRegions[1][3];
 
-        TextureRegion[] rightFrames = new TextureRegion[3];
-        rightFrames[0] = playerSheetRegions[3][0];
-        rightFrames[1] = playerSheetRegions[3][1];
-        rightFrames[2] = playerSheetRegions[3][2];
-        rightAnimation = new Animation<TextureRegion>(animationSpeed, rightFrames);
-        attackRight = playerSheetRegions[3][3];
+        Texture[] downFrames = new Texture[6];
+        downFrames[0] = new Texture("playersprites/Run Down/run_down_1.png");
+        downFrames[1] = new Texture("playersprites/Run Down/run_down_2.png");
+        downFrames[2] = new Texture("playersprites/Run Down/run_down_3.png");
+        downFrames[3] = new Texture("playersprites/Run Down/run_down_4.png");
+        downFrames[4] = new Texture("playersprites/Run Down/run_down_5.png");
+        downFrames[5] = new Texture("playersprites/Run Down/run_down_6.png");
+        downAnimation = new Animation<Texture>(animationSpeed, downFrames);
+
+        TextureRegion[] attackDownFrames = new TextureRegion[4];
+        attackDownFrames[0] = playerAttackSheetRegions[0][0];
+        attackDownFrames[1] = playerAttackSheetRegions[0][1];
+        attackDownFrames[2] = playerAttackSheetRegions[0][2];
+        attackDownFrames[3] = playerAttackSheetRegions[0][3];
+        attackDown = new Animation<TextureRegion>(ATTACK_ANIMATION_SPEED,attackDownFrames);
+
+        Texture[] leftFrames = new Texture[6];
+        leftFrames[0] = new Texture("playersprites/Run Left/run_left_1.png");
+        leftFrames[1] = new Texture("playersprites/Run Left/run_left_2.png");
+        leftFrames[2] = new Texture("playersprites/Run Left/run_left_3.png");
+        leftFrames[3] = new Texture("playersprites/Run Left/run_left_4.png");
+        leftFrames[4] = new Texture("playersprites/Run Left/run_left_5.png");
+        leftFrames[5] = new Texture("playersprites/Run Left/run_left_6.png");
+        leftAnimation = new Animation<Texture>(animationSpeed, leftFrames);
+
+        TextureRegion[] attackLeftFrames = new TextureRegion[4];
+        attackLeftFrames[0] = playerAttackSheetRegions[3][0];
+        attackLeftFrames[1] = playerAttackSheetRegions[3][1];
+        attackLeftFrames[2] = playerAttackSheetRegions[3][2];
+        attackLeftFrames[3] = playerAttackSheetRegions[3][3];
+        attackLeft = new Animation<TextureRegion>(ATTACK_ANIMATION_SPEED,attackLeftFrames);
+
+
+        Texture[] upFrames = new Texture[6];
+        upFrames[0] = new Texture("playersprites/Run Up/run_up_1.png");
+        upFrames[1] = new Texture("playersprites/Run Up/run_up_2.png");
+        upFrames[2] = new Texture("playersprites/Run Up/run_up_3.png");
+        upFrames[3] = new Texture("playersprites/Run Up/run_up_4.png");
+        upFrames[4] = new Texture("playersprites/Run Up/run_up_5.png");
+        upFrames[5] = new Texture("playersprites/Run Up/run_up_6.png");
+        upAnimation = new Animation<Texture>(animationSpeed, upFrames);
+
+        TextureRegion[] attackUpFrames = new TextureRegion[4];
+        attackUpFrames[0] = playerAttackSheetRegions[2][0];
+        attackUpFrames[1] = playerAttackSheetRegions[2][1];
+        attackUpFrames[2] = playerAttackSheetRegions[2][2];
+        attackUpFrames[3] = playerAttackSheetRegions[2][3];
+        attackUp = new Animation<TextureRegion>(ATTACK_ANIMATION_SPEED,attackUpFrames);
+
+        Texture[] rightFrames = new Texture[6];
+        rightFrames[0] = new Texture("playersprites/Run Right/run_right_1.png");
+        rightFrames[1] = new Texture("playersprites/Run Right/run_right_2.png");
+        rightFrames[2] = new Texture("playersprites/Run Right/run_right_3.png");
+        rightFrames[3] = new Texture("playersprites/Run Right/run_right_4.png");
+        rightFrames[4] = new Texture("playersprites/Run Right/run_right_5.png");
+        rightFrames[5] = new Texture("playersprites/Run Right/run_right_6.png");
+        rightAnimation = new Animation<Texture>(animationSpeed, rightFrames);
+
+        TextureRegion[] attackRightFrames = new TextureRegion[4];
+        attackRightFrames[0] = playerAttackSheetRegions[1][0];
+        attackRightFrames[1] = playerAttackSheetRegions[1][1];
+        attackRightFrames[2] = playerAttackSheetRegions[1][2];
+        attackRightFrames[3] = playerAttackSheetRegions[1][3];
+        attackRight = new Animation<TextureRegion>(ATTACK_ANIMATION_SPEED,attackRightFrames);
 
         shouldFlashRed = false;
     }
@@ -349,13 +395,13 @@ public class Player extends Sprite {
 
         if (showAttackAnimation) {
             if (isFacingLeft) {
-                batch.draw(attackLeft, getX(), getY());
+                batch.draw(attackLeft.getKeyFrame(attackTime, false), getX()-attackOffsetX, getY()-attackOffsetY);
             } else if (isFacingRight) {
-                batch.draw(attackRight, getX(), getY());
+                batch.draw(attackRight.getKeyFrame(attackTime, false), getX()-attackOffsetX, getY()-attackOffsetY);
             } else if (isFacingUp) {
-                batch.draw(attackUp, getX(), getY());
+                batch.draw(attackUp.getKeyFrame(attackTime, false), getX()-attackOffsetX, getY()-attackOffsetY);
             } else if (isFacingDown) {
-                batch.draw(attackDown, getX(), getY());
+                batch.draw(attackDown.getKeyFrame(attackTime, false), getX()-attackOffsetX, getY()-attackOffsetY);
             }
         } else {
             if (isRunning) {
