82025828421341680fc1b13f6b3b86ba19ca2363
Alex <ajmarksboy@gmail.com>
2020-10-04 20:10:10 -0500

    Sort of working dodge

--- core/src/entities/Player.java
+++ core/src/entities/Player.java
@@ -47,11 +47,10 @@ public class Player extends Sprite {
         LEFT
     }
 
-    private Texture playerUp = new Texture("player-up.png");
-    private Texture playerDown = new Texture("playersprites/Run Down/run_down_1.png");
-    private Texture playerLeft = new Texture("player-left.png");
-    private Texture playerRight = new Texture("player-right.png");
+
+    private Texture playerDown = new Texture("playersprites/RunDown/run_down_1.png");
     private boolean canAttack = true;
+    private boolean canDodge = true;
     private boolean isRunningLeft = false;
     private boolean isRunningRight = false;
     private boolean isRunningUp = false;
@@ -64,7 +63,6 @@ public class Player extends Sprite {
     private boolean canDropBomb = true;
     private float ATTACK_OFFSET = 0.05f;
     private int SWORD_DAMAGE = 1;
-    private Animation<Texture> attackAnimation;
     private Animation<Texture> downAnimation;
     private Animation<Texture> leftAnimation;
     private Animation<Texture> upAnimation;
@@ -73,15 +71,23 @@ public class Player extends Sprite {
     private Animation<TextureRegion> attackDown;
     private Animation<TextureRegion> attackLeft;
     private Animation<TextureRegion> attackRight;
+    private Animation<TextureRegion> dodgeUp;
+    private Animation<TextureRegion> dodgeDown;
+    private Animation<TextureRegion> dodgeLeft;
+    private Animation<TextureRegion> dodgeRight;
     private float animationSpeed = 0.13f;
     private float attackTime = 0f;
     private float walkTime = 0f;
     private int maxLives = 5;
     private int lives = maxLives;
     private boolean showAttackAnimation = false;
+    private boolean showDodgeAnimation = false;
     private float ATTACK_ANIMATION_SPEED = 0.025f;
     private float ATTACK_COOLDOWN = ATTACK_ANIMATION_SPEED * 13;
     private float ATTACK_ANIMATION_DURATION = 0.2f;
+    private float DODGE_ANIMATION_DURATION = 0.5f;
+    private float DODGE_COOLDOWN = DODGE_ANIMATION_DURATION * 5;
+    private int DODGE_DISTANCE = 30;
     private int bombs = 100;
     private boolean shouldFlashRed;
     private int attackOffsetX = 11;
@@ -122,32 +128,11 @@ public class Player extends Sprite {
     }
 
     public Player(float x, float y, FlashRedManager flashRedManager) {
-        super(new Texture("playersprites/Run Down/run_down_1.png"));
+        super(new Texture("playersprites/RunDown/run_down_1.png"));
         setPosition(x, y);
         setTexture(playerDown);
         isFacingDown = true;
         this.flashRedManager = flashRedManager;
-        Texture[] attackFrames = new Texture[13];
-        attackFrames[0] = new Texture("attack/a1.png");
-        attackFrames[1] = new Texture("attack/a2.png");
-        attackFrames[2] = new Texture("attack/a3.png");
-        attackFrames[3] = new Texture("attack/a4.png");
-        attackFrames[4] = new Texture("attack/a5.png");
-        attackFrames[5] = new Texture("attack/a6.png");
-        attackFrames[6] = new Texture("attack/a7.png");
-        attackFrames[7] = new Texture("attack/a8.png");
-        attackFrames[8] = new Texture("attack/a9.png");
-        attackFrames[9] = new Texture("attack/a10.png");
-        attackFrames[10] = new Texture("attack/a11.png");
-        attackFrames[11] = new Texture("attack/a12.png");
-        attackFrames[12] = new Texture("attack/a13.png");
-        attackAnimation = new Animation<Texture>(ATTACK_ANIMATION_SPEED, attackFrames);
-
-
-        Texture playerSheet = new Texture("player-sheet-all.png");
-        TextureRegion[][] playerSheetRegions = TextureRegion.split(playerSheet,
-                playerSheet.getWidth() / 4,
-                playerSheet.getHeight() / 4);
 
 
         Texture playerAttackSheet = new Texture("playersprites/player_attack_frames.png");
@@ -155,14 +140,20 @@ public class Player extends Sprite {
                 playerAttackSheet.getWidth() / 4,
                 playerAttackSheet.getHeight() / 4);
 
+        Texture playerDodgeSheet = new Texture("playersprites/player_dodge_frames.png");
+        TextureRegion[][] playerDodgeSheetRegions = TextureRegion.split(playerDodgeSheet,
+                playerDodgeSheet.getWidth() / 4,
+                playerDodgeSheet.getHeight() / 4);
+
+
 
         Texture[] downFrames = new Texture[6];
-        downFrames[0] = new Texture("playersprites/Run Down/run_down_1.png");
-        downFrames[1] = new Texture("playersprites/Run Down/run_down_2.png");
-        downFrames[2] = new Texture("playersprites/Run Down/run_down_3.png");
-        downFrames[3] = new Texture("playersprites/Run Down/run_down_4.png");
-        downFrames[4] = new Texture("playersprites/Run Down/run_down_5.png");
-        downFrames[5] = new Texture("playersprites/Run Down/run_down_6.png");
+        downFrames[0] = new Texture("playersprites/RunDown/run_down_1.png");
+        downFrames[1] = new Texture("playersprites/RunDown/run_down_2.png");
+        downFrames[2] = new Texture("playersprites/RunDown/run_down_3.png");
+        downFrames[3] = new Texture("playersprites/RunDown/run_down_4.png");
+        downFrames[4] = new Texture("playersprites/RunDown/run_down_5.png");
+        downFrames[5] = new Texture("playersprites/RunDown/run_down_6.png");
         downAnimation = new Animation<Texture>(animationSpeed, downFrames);
 
         TextureRegion[] attackDownFrames = new TextureRegion[4];
@@ -172,14 +163,22 @@ public class Player extends Sprite {
         attackDownFrames[3] = playerAttackSheetRegions[0][3];
         attackDown = new Animation<TextureRegion>(ATTACK_ANIMATION_SPEED,attackDownFrames);
 
+        TextureRegion[] dodgeDownFrames = new TextureRegion[4];
+        dodgeDownFrames[0] = playerDodgeSheetRegions[0][0];
+        dodgeDownFrames[1] = playerDodgeSheetRegions[0][1];
+        dodgeDownFrames[2] = playerDodgeSheetRegions[0][2];
+        dodgeDownFrames[3] = playerDodgeSheetRegions[0][3];
+        dodgeDown = new Animation<TextureRegion>(DODGE_ANIMATION_DURATION,dodgeDownFrames);
+
         Texture[] leftFrames = new Texture[6];
-        leftFrames[0] = new Texture("playersprites/Run Left/run_left_1.png");
-        leftFrames[1] = new Texture("playersprites/Run Left/run_left_2.png");
-        leftFrames[2] = new Texture("playersprites/Run Left/run_left_3.png");
-        leftFrames[3] = new Texture("playersprites/Run Left/run_left_4.png");
-        leftFrames[4] = new Texture("playersprites/Run Left/run_left_5.png");
-        leftFrames[5] = new Texture("playersprites/Run Left/run_left_6.png");
+        leftFrames[0] = new Texture("playersprites/RunLeft/run_left_1.png");
+        leftFrames[1] = new Texture("playersprites/RunLeft/run_left_2.png");
+        leftFrames[2] = new Texture("playersprites/RunLeft/run_left_3.png");
+        leftFrames[3] = new Texture("playersprites/RunLeft/run_left_4.png");
+        leftFrames[4] = new Texture("playersprites/RunLeft/run_left_5.png");
+        leftFrames[5] = new Texture("playersprites/RunLeft/run_left_6.png");
         leftAnimation = new Animation<Texture>(animationSpeed, leftFrames);
+
 
         TextureRegion[] attackLeftFrames = new TextureRegion[4];
         attackLeftFrames[0] = playerAttackSheetRegions[3][0];
@@ -188,14 +187,21 @@ public class Player extends Sprite {
         attackLeftFrames[3] = playerAttackSheetRegions[3][3];
         attackLeft = new Animation<TextureRegion>(ATTACK_ANIMATION_SPEED,attackLeftFrames);
 
+        TextureRegion[] dodgeLeftFrames = new TextureRegion[4];
+        dodgeLeftFrames[0] = playerDodgeSheetRegions[3][0];
+        dodgeLeftFrames[1] = playerDodgeSheetRegions[3][1];
+        dodgeLeftFrames[2] = playerDodgeSheetRegions[3][2];
+        dodgeLeftFrames[3] = playerDodgeSheetRegions[3][3];
+        dodgeLeft = new Animation<TextureRegion>(DODGE_ANIMATION_DURATION,dodgeDownFrames);
+
 
         Texture[] upFrames = new Texture[6];
-        upFrames[0] = new Texture("playersprites/Run Up/run_up_1.png");
-        upFrames[1] = new Texture("playersprites/Run Up/run_up_2.png");
-        upFrames[2] = new Texture("playersprites/Run Up/run_up_3.png");
-        upFrames[3] = new Texture("playersprites/Run Up/run_up_4.png");
-        upFrames[4] = new Texture("playersprites/Run Up/run_up_5.png");
-        upFrames[5] = new Texture("playersprites/Run Up/run_up_6.png");
+        upFrames[0] = new Texture("playersprites/RunUp/run_up_1.png");
+        upFrames[1] = new Texture("playersprites/RunUp/run_up_2.png");
+        upFrames[2] = new Texture("playersprites/RunUp/run_up_3.png");
+        upFrames[3] = new Texture("playersprites/RunUp/run_up_4.png");
+        upFrames[4] = new Texture("playersprites/RunUp/run_up_5.png");
+        upFrames[5] = new Texture("playersprites/RunUp/run_up_6.png");
         upAnimation = new Animation<Texture>(animationSpeed, upFrames);
 
         TextureRegion[] attackUpFrames = new TextureRegion[4];
@@ -205,13 +211,20 @@ public class Player extends Sprite {
         attackUpFrames[3] = playerAttackSheetRegions[2][3];
         attackUp = new Animation<TextureRegion>(ATTACK_ANIMATION_SPEED,attackUpFrames);
 
+        TextureRegion[] dodgeUpFrames = new TextureRegion[4];
+        dodgeUpFrames[0] = playerDodgeSheetRegions[2][0];
+        dodgeUpFrames[1] = playerDodgeSheetRegions[2][1];
+        dodgeUpFrames[2] = playerDodgeSheetRegions[2][2];
+        dodgeUpFrames[3] = playerDodgeSheetRegions[2][3];
+        dodgeUp = new Animation<TextureRegion>(DODGE_ANIMATION_DURATION,dodgeUpFrames);
+
         Texture[] rightFrames = new Texture[6];
-        rightFrames[0] = new Texture("playersprites/Run Right/run_right_1.png");
-        rightFrames[1] = new Texture("playersprites/Run Right/run_right_2.png");
-        rightFrames[2] = new Texture("playersprites/Run Right/run_right_3.png");
-        rightFrames[3] = new Texture("playersprites/Run Right/run_right_4.png");
-        rightFrames[4] = new Texture("playersprites/Run Right/run_right_5.png");
-        rightFrames[5] = new Texture("playersprites/Run Right/run_right_6.png");
+        rightFrames[0] = new Texture("playersprites/RunRight/run_right_1.png");
+        rightFrames[1] = new Texture("playersprites/RunRight/run_right_2.png");
+        rightFrames[2] = new Texture("playersprites/RunRight/run_right_3.png");
+        rightFrames[3] = new Texture("playersprites/RunRight/run_right_4.png");
+        rightFrames[4] = new Texture("playersprites/RunRight/run_right_5.png");
+        rightFrames[5] = new Texture("playersprites/RunRight/run_right_6.png");
         rightAnimation = new Animation<Texture>(animationSpeed, rightFrames);
 
         TextureRegion[] attackRightFrames = new TextureRegion[4];
@@ -220,6 +233,14 @@ public class Player extends Sprite {
         attackRightFrames[2] = playerAttackSheetRegions[1][2];
         attackRightFrames[3] = playerAttackSheetRegions[1][3];
         attackRight = new Animation<TextureRegion>(ATTACK_ANIMATION_SPEED,attackRightFrames);
+
+        TextureRegion[] dodgeRightFrames = new TextureRegion[4];
+        dodgeRightFrames[0] = playerDodgeSheetRegions[1][0];
+        dodgeRightFrames[1] = playerDodgeSheetRegions[1][1];
+        dodgeRightFrames[2] = playerDodgeSheetRegions[1][2];
+        dodgeRightFrames[3] = playerDodgeSheetRegions[1][3];
+        dodgeRight = new Animation<TextureRegion>(DODGE_ANIMATION_DURATION,dodgeRightFrames);
+
 
         shouldFlashRed = false;
     }
@@ -314,6 +335,48 @@ public class Player extends Sprite {
         }
     }
 
+    public void dodge(){
+        if(!canDodge) return;
+        canAttack = false;
+        canDodge = false;
+        attackTime = 0;
+        showDodgeAnimation = true;
+
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                showDodgeAnimation = false;
+            }
+        }, DODGE_ANIMATION_DURATION);
+
+        if (isFacingLeft) {
+            setX(getX() - DODGE_DISTANCE);
+
+        } else if (isFacingRight) {
+            setX(getX() + DODGE_DISTANCE);
+
+        } else if (isFacingUp) {
+            setY(getY() + DODGE_DISTANCE);
+
+        } else if (isFacingDown) {
+            setY(getY() - DODGE_DISTANCE);
+        }
+
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                canAttack = true;
+            }
+        }, ATTACK_COOLDOWN);
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                canDodge = true;
+            }
+        }, DODGE_COOLDOWN);
+
+    }
+
     public boolean hasBombs() {
         return bombs > 0;
     }
@@ -332,6 +395,13 @@ public class Player extends Sprite {
                 showAttackAnimation = false;
             }
         }, ATTACK_ANIMATION_DURATION);
+
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                canAttack = true;
+            }
+        }, ATTACK_COOLDOWN);
 
         Timer.schedule(new Timer.Task() {
             @Override
@@ -392,7 +462,17 @@ public class Player extends Sprite {
             } else if (isFacingDown) {
                 batch.draw(attackDown.getKeyFrame(attackTime, false), getX()-attackOffsetX, getY()-attackOffsetY);
             }
-        } else {
+        } else if(showDodgeAnimation){
+            if (isFacingLeft) {
+                batch.draw(dodgeLeft.getKeyFrame(attackTime, false), getX(), getY());
+            } else if (isFacingRight) {
+                batch.draw(dodgeRight.getKeyFrame(attackTime, false), getX(), getY());
+            } else if (isFacingUp) {
+                batch.draw(dodgeUp.getKeyFrame(attackTime, false), getX(), getY());
+            } else if (isFacingDown) {
+                batch.draw(dodgeDown.getKeyFrame(attackTime, false), getX(), getY());
+            }
+        }else {
             if (isRunning) {
                 if (strifeDirection != Direction.None) {
                     if (strifeDirection == Direction.Up) {
